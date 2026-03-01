7d23bcc0a29be67e4276a69a570f831346634898
junojsx <junojsx@gmail.com>
2026-02-28 22:36:34 -0800

    adding projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -16,38 +16,30 @@ export const projects: Project[] = [
     description:
       "A full-stack Netflix clone that replicates the core streaming experience with authentication, dynamic content browsing, and a responsive, modern UI. Built to showcase real-world frontend architecture, API integration, and scalable design.",
     techTags: ["React", "TypeScript", "Storybook", "Radix UI"],
-    liveUrl: "https://example.com",
-    repoUrl: "https://github.com/yourusername/focuscraft",
+    liveUrl: "https://dupeflix.vercel.app/",
+    repoUrl: "https://github.com/junojsx/dupeflix",
   },
   {
     id: "project-3",
-    name: "DupeFlix",
+    name: "For Yohan",
     description:
-      "A full-stack Netflix clone that replicates the core streaming experience with authentication, dynamic content browsing, and a responsive, modern UI. Built to showcase real-world frontend architecture, API integration, and scalable design.",
-    techTags: ["React", "TypeScript", "Storybook", "Radix UI"],
-    liveUrl: "https://example.com",
-    repoUrl: "https://github.com/yourusername/focuscraft",
+      "A text to speech app I created for my non-verbal little cousin, Yohan. The reason why I built it was to help her communicate more effectively and express her thoughts and feelings in a way that was accessible to her. I saw how much she struggled to communicate with others and wanted to create something that could help her connect with the world around her. The app allows her to type out messages and have them spoken aloud, making it easier for her to communicate with family, friends, and caregivers. It was a rewarding experience to see how much the app has improved her ability to express herself and connect with others. I hope that it continues to make a positive impact on her life and the lives of others who may benefit from similar technology. Thus, I pursued a career in tech to create more tools that can help people like Yohan and make a positive impact on their lives.",
+    techTags: ["React", "Bootstrap", "Speech Synthesis API"],
+    liveUrl: "https://tangerine-syrniki-087126.netlify.app/",
+    repoUrl: "https://github.com/junojsx/for-Yohan",
   },
   {
     id: "project-4",
-    name: "DupeFlix",
+    name: "CPACC Flashcards",
     description:
       "A full-stack Netflix clone that replicates the core streaming experience with authentication, dynamic content browsing, and a responsive, modern UI. Built to showcase real-world frontend architecture, API integration, and scalable design.",
     techTags: ["React", "TypeScript", "Storybook", "Radix UI"],
-    liveUrl: "https://example.com",
-    repoUrl: "https://github.com/yourusername/focuscraft",
+    liveUrl:
+      "https://app.netlify.com/projects/gleeful-hummingbird-1d3528/overview",
+    repoUrl: "https://github.com/junojsx/cpacc-flashcards",
   },
   {
     id: "project-5",
-    name: "DupeFlix",
-    description:
-      "A full-stack Netflix clone that replicates the core streaming experience with authentication, dynamic content browsing, and a responsive, modern UI. Built to showcase real-world frontend architecture, API integration, and scalable design.",
-    techTags: ["React", "TypeScript", "Storybook", "Radix UI"],
-    liveUrl: "https://example.com",
-    repoUrl: "https://github.com/yourusername/focuscraft",
-  },
-  {
-    id: "project-6",
     name: "Keylometer",
     description:
       "For my first project in this course, I decided to build my version of a speed typing challenge. As a novice mechanical keyboard enthusiast, I enjoy building keyboards using different switches depending on what sound profile and feel I am looking for at certain times. And to test out the build, I always use websites like monkeytype and kbsim.",
