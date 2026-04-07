d1c3d17d3d8c511090c5a08773bf00ca634144ce
junojsx <junojsx@gmail.com>
2026-04-06 22:54:17 -0700

    fixing non visible cards

--- src/components/sections/Blog.tsx
+++ src/components/sections/Blog.tsx
@@ -59,11 +59,11 @@ export default function Blog({ standalone = false }: BlogProps) {
     <section
       id="blog"
       aria-labelledby="blog-heading"
-      className="relative min-h-[calc(100vh-8rem)] overflow-hidden text-dark-gray"
+      className="relative z-0 min-h-[calc(100vh-8rem)] overflow-hidden text-dark-gray"
       style={{ backgroundColor: PAGE_BG }}
     >
       <div
-        className="pointer-events-none absolute inset-0 opacity-[0.4]"
+        className="pointer-events-none absolute inset-0 -z-10 opacity-[0.4]"
         style={{
           backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 256 256' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='n'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.75' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23n)' opacity='0.05'/%3E%3C/svg%3E")`,
           backgroundSize: "200px 200px",
